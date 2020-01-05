9e22f63f38afe2ebad0ef78f264f36c1055f221f
Blake Fogle <foglebd@gmail.com>
2020-01-04 22:45:29 -0800

    node express handlebars burgers app created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('The Fonz');
+INSERT INTO burgers (burger_name) VALUES ('The Crepe');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mondays', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Corbin Bleu', true);
